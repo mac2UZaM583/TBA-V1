dda0f378c2a02df59b6b3aa43345f1362bf78448
mac2UZaM583 <163586801+mac2UZaM583@users.noreply.github.com>
2024-10-22 13:27:33 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ from onion.domain.g_settings_bt import settings_
 from onion.app.infrastructure.g_df_test import g_df_test
 from onion.infrastructure.visual import g_visualize
 
+import numpy as np
+
 def main():
+    # from onion.app.domain.g_.global_ import g_symbols_f
+    # g_symbols_f()
+    # все в цикле плю
     data = g_df_test(settings_)
-    print(data[["BT", "BT/ balance"]])
+    # print(data[["BT", "BT/ balance"]])
 
     g_visualize(
         x=data.index,
         y=data["close"],
         markers=(data["predicted_label"], data["BT"]),
         markers_settings=[
-            [
+            (
                 dict(
                     class_=-1,
                     color="red",
@@ -22,7 +27,7 @@ def main():
                     color="green",
                     name="Buy",
                 )
-            ],
+            ),
             (
                 dict(
                     class_=1,
@@ -34,14 +39,29 @@ def main():
                     color="pink",
                     name="not_in_position"
                 ),
-                dict(
-                    class_=2,
-                    color="yellow",
-                    name="avg_order"
-                )
-            )
-        ]
+                # dict(
+                #     class_=2,
+                #     color="yellow",
+                #     name="avg_order"
+                # )
+            ),
+        ],
+        # add=(
+        #     dict(
+        #         x=np.arange(data["BT/ balance"].count()),
+        #         y=data["BT/ balance"].iloc[-data["BT/ balance"].count():],
+        #         name="balance",
+        #     ),
+        # )
     )
-
+    g_visualize(
+        x=np.arange(data["BT/ balance"].count()),
+        y=data["BT/ balance"].iloc[-data["BT/ balance"].count():],
+    )
+    # print(data["BT/ balance"].iloc[-data["BT/ balance"].count():])
 if __name__ == "__main__":
     main()
+
+# настроить pnl_percent
+# отображение баланса
+#
